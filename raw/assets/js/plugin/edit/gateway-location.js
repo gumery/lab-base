867212e327af918063bb68bc609369dd0f45b2b9
Jinlin Li <jinlin.li@geneegroup.com>
2017-08-23 18:03:04 +0800

    手机扫码调用这个view 不适用selectpicker

--- raw/assets/js/plugin/edit/gateway-location.js
+++ raw/assets/js/plugin/edit/gateway-location.js
@@ -1,29 +1,40 @@
-define('plugin/edit/gateway-location', ['jquery', 'bootstrap', 'bootbox', 'bootstrap-select'], function($, Bootstrap, Bootbox, BootSelect) {
+define('plugin/edit/gateway-location', ['jquery', 'bootstrap', 'bootbox', 'bootstrap-select'], function($, Bootstrap, Bootbox, BootSelect) {     
+    var checkMobile;
+    if (typeof window.orientation == 'undefined') {
+        checkMobile = false;
+    }
+    else {
+        checkMobile = true;
+    }
     $('body').on('change', '.app-location-select-courier', function() {
         var $that = $(this);
         var kk = $that.attr('name');
-		var pk = /\[([a-zA-Z0-9]*)\]/.exec(kk);
-		if (pk) {
-			var ki = pk[1];
-		}
+        var pk = /\[([a-zA-Z0-9]*)\]/.exec(kk);
+        if (pk) {
+            var ki = pk[1];
+        }
         var kclass= ['.', $that.attr('data-sub-class')].join('');
         var kurl = $that.attr('data-sub-url');
         var loadingHTML = '<div><div class="control-label col-sm-3"></div><div class="col-sm-9"><i class="fa fa-spinner fa-spin fa-2x"></i></div></div>';
         var $myContainer = $that.parents('form').find(kclass);
         if (!$myContainer.length) return;
         $myContainer.html(loadingHTML);
-		var data = {
+        var data = {
             'value': $that.val()
         };
-		if (undefined!==ki) {
-			data['multiKey'] = ki;
-		}
+        if (undefined!==ki) {
+            data['multiKey'] = ki;
+        }
         $.get(kurl, data, function(result) {
             $myContainer.html(result);
             var $pm = $myContainer.parents('.modal');
             $pm.length && $pm.modal('handleUpdate');
-            $('.selectpicker').selectpicker('refresh');
+            if (!checkMobile) {
+                $('.selectpicker').selectpicker('refresh');
+            }
         });
     });
-    $('.selectpicker').selectpicker();
+    if (!checkMobile) {
+        $('.selectpicker').selectpicker();
+    }
 });
