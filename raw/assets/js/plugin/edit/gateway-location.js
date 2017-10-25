a154800db85aef2d722f293b5379f5109bba5ccf
gumery <jsjshan123@163.com>
2017-10-25 12:10:41 +0800

    代码缩进问题

--- raw/assets/js/plugin/edit/gateway-location.js
+++ raw/assets/js/plugin/edit/gateway-location.js
@@ -14,17 +14,17 @@ define('plugin/edit/gateway-location', ['jquery', 'bootstrap', 'bootbox', 'boots
             var ki = pk[1];
         }
         var kclass = $that.attr('data-sub-class');
-    if (!kclass) return;
+        if (!kclass) return;
         kclass= ['.', kclass].join('');
         var kurl = $that.attr('data-sub-url');
-    var across = $that.attr('data-is-across');
+        var across = $that.attr('data-is-across');
         var loadingHTML = '<div><div class="control-label col-sm-3"></div><div class="col-sm-9"><i class="fa fa-spinner fa-spin fa-2x"></i></div></div>';
         var $myContainer = $that.parents('form').find(kclass);
         if (!$myContainer.length) return;
         $myContainer.html(loadingHTML);
         var data = {
             'value': $that.val(),
-        'across': across
+            'across': across
         };
         if (undefined!==ki) {
             data['multiKey'] = ki;
