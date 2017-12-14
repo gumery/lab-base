d1c60740b12d55a2adff6329a983a97897922d11
洪小兵 <pihizi@msn.com>
2017-12-14 10:14:43 +0000

    with login info

--- class/Gini/Module/LabBase.php
+++ class/Gini/Module/LabBase.php
@@ -8,7 +8,7 @@ class LabBase
     {
     }
 
-    public static function getFEUrl($path, $justPath = false)
+    public static function getFEUrl($path, $justPath = false, $withLoginInfo=false)
     {
         $feURL = \Gini\Config::get('app.lab-fe-url');
         if (!$feURL || $feURL=='${LAB_FE_URL}') {
@@ -16,27 +16,43 @@ class LabBase
         }
 
         $url = rtrim("{$feURL}/{$path}", '/');
-	if (!$justPath) return $url;
-	$uri = parse_url($url);
-	return $uri['path'];
+        if (!$justPath) {
+            if ($withLoginInfo) {
+                $me = _G('ME');
+                if ($me->id) {
+                    $clientID = \Gini\Config::get('app.lab-fe-app-client-id');
+                    $gapperToken = \Gini\Gapper\Client::getLoginToken($clientID);
+                    if ($gapperToken) {
+                        $group = _G('GROUP');
+                        $url = \Gini\URI::url($feURL."/gapper/client/login", [
+                            'gapper-token'=> $gapperToken,
+                            'gapper-group'=> $group->id,
+                            'redirect'=> $url
+                        ]);
+                    }
+                }
+            }
+            return $url;
+        }
+        $uri = parse_url($url);
+        return $uri['path'];
     }
 
     public static function getRedirectUrl($path, $clientID=null)
     {
-	$app = \Gini\Gapper\Client::getInfo();
+        $app = \Gini\Gapper\Client::getInfo();
         $clientID = $clientID?:\Gini\Gapper\Client::getId();
-	$to = \Gini\Gapper\Client::getInfo($clientID);
-	$url = $to['url'] . "/" . ltrim($path, '/');
+        $to = \Gini\Gapper\Client::getInfo($clientID);
+        $url = $to['url'] . "/" . ltrim($path, '/');
 
         $result = "gapper/client/go/{$clientID}";
-	$group = _G('GROUP');
-	if ($group->id) {
-		$result .= "/{$group->id}";
-	}
+        $group = _G('GROUP');
+        if ($group->id) {
+            $result .= "/{$group->id}";
+        }
 
         return \Gini\URI::url("{$app['url']}/{$result}", [
-		'redirect'=> $url
-	]);
+        'redirect'=> $url
+    ]);
     }
 }
-
