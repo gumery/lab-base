9f9cb7340085632af4ea2aafecc204ec3aa52702
yuxiang.gao <jsjshan123@163.com>
2018-02-26 14:52:41 +0800

    代码修改（yml配置文件里面添加配置，使用回调方法，在LabBase.php文件里面进行权限判断）

--- class/Gini/Controller/CGI/Rest/Base.php
+++ class/Gini/Controller/CGI/Rest/Base.php
@@ -176,7 +176,7 @@ class Base extends \Gini\Controller\REST
         foreach($subs as $client_id => $sub) {
             foreach($sub as $id => $item) {
                 $subs[$client_id][$id]['url'] = self::_getFEURL($item['url'], $client_id);
-                if (!$me->isAllowedTo('订单支付', 'order') && $item['title'] == '付款管理'){
+                if (!empty($item['url_check_callback']) && call_user_func($item['url_check_callback']) === false) {
                     unset($subs[$client_id][$id]);
                 }
             }
